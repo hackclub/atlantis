fd8057150d596828aab866e7614236f527470933
swaritnarang <narangswarit02@gmail.com>
2026-06-08 21:37:04 -0500

    Internal layers field

--- layered/layered_site/models.py
+++ layered/layered_site/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="hackclub_profile")
 	verification_status = models.CharField(max_length=64, blank=True, default="")
 	slack_id = models.CharField(max_length=64, blank=True, default="")
+	layers = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.user.username
