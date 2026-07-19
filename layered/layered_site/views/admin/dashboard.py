50c80bf5c3bacaa01b7dc67d3a6b589786c7b1cc
swaritnarang <narangswarit02@gmail.com>
2026-07-18 23:13:51 -0500

    cleanup code some more

--- layered/layered_site/views/admin/dashboard.py
+++ layered/layered_site/views/admin/dashboard.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
 from django.contrib.admin.views.decorators import staff_member_required
 
-from ...models import Project, Ship
 from ..helpers import check_perms
 
 @staff_member_required
